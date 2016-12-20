f754bfa812cdfbdc3229c5e0bd49903362df1675
Bert Spaan <bert.spaan@gmail.com>
2016-12-20 14:34:05 -0500

    removed react-intl, added git-dir-deploy

--- app/app.js
+++ app/app.js
@@ -6,12 +6,6 @@
  */
 import 'babel-polyfill';
 
-/* eslint-disable import/no-unresolved, import/extensions */
-// Load the manifest.json file and the .htaccess file
-import '!file?name=[name].[ext]!./manifest.json';
-import 'file?name=[name].[ext]!./.htaccess';
-/* eslint-enable import/no-unresolved, import/extensions */
-
 // Import all the third party stuff
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -19,11 +13,11 @@ import { Provider } from 'react-redux';
 import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { useScroll } from 'react-router-scroll';
-import LanguageProvider from 'containers/LanguageProvider';
+// import LanguageProvider from 'containers/LanguageProvider';
 import configureStore from './store';
 
 // Import i18n messages
-import { translationMessages } from './i18n';
+// import { translationMessages } from './i18n';
 
 // Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
 import 'sanitize.css/sanitize.css';
@@ -51,54 +45,48 @@ const rootRoute = {
   childRoutes: createRoutes(store),
 };
 
-
-const render = (translatedMessages) => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <LanguageProvider messages={translatedMessages}>
-        <Router
-          history={history}
-          routes={rootRoute}
-          render={
-            // Scroll to top when going to a new page, imitating default browser
-            // behaviour
-            applyRouterMiddleware(useScroll())
-          }
-        />
-      </LanguageProvider>
-    </Provider>,
-    document.getElementById('app')
-  );
-};
-
+ReactDOM.render(
+  <Provider store={store}>
+    <Router
+      history={history}
+      routes={rootRoute}
+      render={
+        // Scroll to top when going to a new page, imitating default browser
+        // behaviour
+        applyRouterMiddleware(useScroll())
+      }
+    />
+  </Provider>,
+  document.getElementById('app')
+);
 
 // Hot reloadable translation json files
-if (module.hot) {
-  // modules.hot.accept does not accept dynamic dependencies,
-  // have to be constants at compile-time
-  module.hot.accept('./i18n', () => {
-    render(translationMessages);
-  });
-}
+// if (module.hot) {
+//   // modules.hot.accept does not accept dynamic dependencies,
+//   // have to be constants at compile-time
+//   module.hot.accept('./i18n', () => {
+//     render(translationMessages);
+//   });
+// }
 
 // Chunked polyfill for browsers without Intl support
-if (!window.Intl) {
-  (new Promise((resolve) => {
-    resolve(System.import('intl'));
-  }))
-    .then(() => Promise.all([
-      System.import('intl/locale-data/jsonp/de.js'),
-    ]))
-    .then(() => render(translationMessages))
-    .catch((err) => {
-      throw err;
-    });
-} else {
-  render(translationMessages);
-}
+// if (!window.Intl) {
+//   (new Promise((resolve) => {
+//     resolve(System.import('intl'));
+//   }))
+//     .then(() => Promise.all([
+//       System.import('intl/locale-data/jsonp/de.js'),
+//     ]))
+//     .then(() => render(translationMessages))
+//     .catch((err) => {
+//       throw err;
+//     });
+// } else {
+//   render(translationMessages);
+// }
 
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
 // we do not want it installed
-import { install } from 'offline-plugin/runtime';
-install();
+// import { install } from 'offline-plugin/runtime';
+// install();
